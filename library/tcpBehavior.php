20834bc06ff0c7aa667f66529e850cda9fb3473f
waynesun01 <waynesun01@gmail.com>
2017-05-04 18:18:01 +0800

    tcp service

---
+++ library/tcpBehavior.php
@@ -0,0 +1,15 @@
+<?php
+namespace waynesun\swoole;
+
+/**
+ * Description of tcpBehavior
+ *
+ * @author WayneSun
+ */
+abstract class tcpBehavior extends behavior {
+
+    public function onReceive($server, $fd, $from_id, $data) {
+        
+    }
+
+}
